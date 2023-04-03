bc0f665706a485d27482aadce5467337f113e1e7
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-03 02:15:15 +0200

    Add Account Log in End-point

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using API.DTOs;
+using Core.Entities.Identity;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IJwtService jwtService;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            IJwtService jwtService)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+            this.jwtService = jwtService;
+        }
+        // POST: api/account
+        [HttpPost("login")]
+        public async Task<ActionResult<TokenDto>> Login([FromBody]LoginDto loginDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest("Invalid input.");
+            }
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if(user == null)
+            {
+                return Unauthorized(loginDto);
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if(!result.Succeeded)
+            {
+                return Unauthorized(loginDto);
+            }
+            // Token Generation is not implemented yet.
+            var tokenDto = new TokenDto()
+            {
+                Email = user.Email,
+                Token = jwtService.CreatToken(user)
+            };
+            return Ok(tokenDto);
+        }
+    }
+}
---
+++ API/DTOs/TokenDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class TokenDto
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Token { get; set; }
+    }
+}
