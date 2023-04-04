81fbf3df2045b735766ebcb27288dc160db27fdc
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-04 05:00:06 +0200

    Add Password Register Validation In The Controller

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,6 +11,8 @@ namespace API.DTOs
         [Required, EmailAddress]
         public string Email { get; set; }
         [Required, DataType(DataType.Password)]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+            ErrorMessage = "Password must have at least 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and at least 6 characters")]
         public string Password { get; set; }
     }
 }
