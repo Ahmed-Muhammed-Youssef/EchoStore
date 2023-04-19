5e8d7de38e49d5d533770aa5447a4ef2ec707f3c
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-19 04:20:55 +0200

    Add Order Entity

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order
+    {
+        public Order()
+        {
+        }
+
+        public Order(string buyerEmail, Address shipToAddress, DeliveryMethod deliveryMethod, IReadOnlyList<OrderItem> orderItems, decimal subtotal)
+        {
+            BuyerEmail = buyerEmail ?? throw new ArgumentNullException(nameof(buyerEmail));
+            ShipToAddress = shipToAddress ?? throw new ArgumentNullException(nameof(shipToAddress));
+            DeliveryMethod = deliveryMethod ?? throw new ArgumentNullException(nameof(deliveryMethod));
+            OrderItems = orderItems ?? throw new ArgumentNullException(nameof(orderItems));
+            Subtotal = subtotal;
+        }
+
+        public int Id { get; set; }
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        public Address ShipToAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public string PaymentIntentId { get; set; }
+        public decimal GetTotal()
+        {
+            return Subtotal + DeliveryMethod.Price;
+        }
+    }
+}
