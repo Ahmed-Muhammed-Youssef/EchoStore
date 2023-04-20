8319bfb8519aed5e6108d017e3b34c9f5cea7de0
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-20 10:57:00 +0200

    Add order entities configurations (#1)

---
+++ Infrastructure/Data/Configurations/OrderConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class OrderConfigurations : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.OwnsOne(O => O.ShipToAddress, a => { a.WithOwner(); });
+            builder.Property(o => o.Status)
+                .HasConversion<string>(s => s.ToString(), s => (OrderStatus)Enum.Parse(typeof(OrderStatus), s));
+            builder.HasMany(o => o.OrderItems)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+    }
+}
---
+++ Infrastructure/Data/Configurations/OrderItemConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class OrderItemConfigurations : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.OwnsOne(oi => oi.ItemOrdered, i => { i.WithOwner(); });
+            builder.Property(oi => oi.Price)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
