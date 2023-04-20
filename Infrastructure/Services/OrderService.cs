39912d0e22ef3ad8aeeb3d0caa99c2659b8c5c56
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-20 12:39:00 +0200

    Add order service interface #4

---
+++ Infrastructure/Services/OrderService.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string cartId, Address shippingAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(string cartId, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
