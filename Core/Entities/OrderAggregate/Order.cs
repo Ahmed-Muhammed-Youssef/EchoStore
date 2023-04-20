f16bc3bfbec8d2bcdebc75f57e708489ea63d8ab
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-20 11:40:48 +0200

    Document Order Entity

--- Core/Entities/OrderAggregate/Order.cs
+++ Core/Entities/OrderAggregate/Order.cs
@@ -22,11 +22,15 @@ namespace Core.Entities.OrderAggregate
         public string BuyerEmail { get; set; }
         public DateTimeOffset OrderDate { get; set; }
         public Address ShipToAddress { get; set; }
-        public DeliveryMethod DeliveryMethod { get; set; }
-        public IReadOnlyList<OrderItem> OrderItems { get; set; }
         public decimal Subtotal { get; set; }
         public OrderStatus Status { get; set; } = OrderStatus.Pending;
         public string PaymentIntentId { get; set; }
+
+        // Navigation Properties
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+
+        // Utility
         public decimal GetTotal()
         {
             return Subtotal + DeliveryMethod.Price;
