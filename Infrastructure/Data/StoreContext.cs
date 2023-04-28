c508268f63a3871e95ff3bbaf4cb07b418e8096a
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-28 17:45:05 +0300

    Migrate Store Db

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -15,7 +15,7 @@ namespace Infrastructure.Data
         public DbSet<ProductBrand> ProductBrands { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
         public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<OrderedProductInfo> OrderedProductInfo { get; set; }
         public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
