6757879025d12264757d5527af7645cb222a128c
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-28 17:45:32 +0300

    Add Order Repository

---
+++ Core/Interfaces/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IOrderRepository
+    {
+        public Task<IReadOnlyList<Order>> GetCurrentUserOrdersAsync (string userEmail);
+        public Task<Order> GetOrderAsync (int orderId);
+        public Task<Order> CreateOrderAsync(Order order);
+        public Task<Order> UpdateOrderAsync(Order order);
+        public Task<Order> DeleteOrderAsync(Order order);
+    }
+}
