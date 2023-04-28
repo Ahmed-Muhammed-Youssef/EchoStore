14827203f39e6472cd6a219bcb329f7aa090876d
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-28 15:46:21 +0300

    Add DeliveryMethod Repository (#4)

---
+++ Infrastructure/Data/DeliveryMethodRepository.cs
@@ -0,0 +1,51 @@
+﻿using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class DeliveryMethodRepository : IDeliveryMethodRepository
+    {
+        private readonly StoreContext _storeContext;
+
+        public DeliveryMethodRepository(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        public async Task<DeliveryMethod> AddAsync(DeliveryMethod delivery)
+        {
+            var res = await _storeContext.DeliveryMethods.AddAsync(delivery);
+            return res.Entity;
+        }
+
+        public async Task<IReadOnlyList<DeliveryMethod>> GetAllAsync()
+        {
+            return await _storeContext
+               .DeliveryMethods
+               .AsNoTracking()
+               .ToListAsync();
+        }
+
+        public async Task<DeliveryMethod> GetByIdAsync(int id)
+        {
+            return await _storeContext.DeliveryMethods.FindAsync(id);
+        }
+
+
+        public async Task<DeliveryMethod> RemoveAsync(int deliveryMethodId)
+        {
+            var dm = await GetByIdAsync(deliveryMethodId);
+            if (dm == null)
+            {
+                throw new Exception("Not Found");
+            }
+            var res = _storeContext.DeliveryMethods.Remove(dm);
+            _storeContext.SaveChanges();
+            return res.Entity;
+        }
+    }
+}
