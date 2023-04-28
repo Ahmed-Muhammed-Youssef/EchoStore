773aa46c09fa8ce5b55beee511870a73722cce12
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-28 17:46:11 +0300

    Add Order Creation Functionality (#4)

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -8,9 +8,46 @@ namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string cartId, Address shippingAddress)
+        private readonly ICartRepository _cartRepository;
+        private readonly IProductRepository _productRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IDeliveryMethodRepository _deliveryMethodRepository;
+
+        public OrderService(ICartRepository cartRepository, IProductRepository productRepository, IOrderRepository orderRepository, IDeliveryMethodRepository deliveryMethodRepository)
         {
-            throw new NotImplementedException();
+            _cartRepository = cartRepository;
+            _productRepository = productRepository;
+            _orderRepository = orderRepository;
+            _deliveryMethodRepository = deliveryMethodRepository;
+        }
+        public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string cartId, Address shippingAddress)
+        {
+            var cart = await _cartRepository.GetCart(cartId);
+            var dm = await _deliveryMethodRepository.GetByIdAsync(deliveryMethodId);
+            if(dm == null)
+            {
+                throw new Exception("404: Delivery method not found");
+            }
+            var items = new List<OrderedProductInfo>();
+            decimal subtotal = 0;
+            foreach (var item in cart.Items)
+            {
+                var productItemInfo = await _productRepository.GetProductInfoByIdAsync(item.Id);
+                // check the quantity availability
+                if (item.Quantity > productItemInfo.AvailableAmount)
+                {
+                    throw new Exception($"Not enough available amount. Only available {productItemInfo.AvailableAmount}");
+                }
+                
+                var orderedItem = new OrderedProductInfo(item.Quantity);
+
+                subtotal += productItemInfo.Price * item.Quantity;
+                items.Add(orderedItem);
+            }
+            var order = new Order(buyerEmail, shippingAddress, dm, items, subtotal);
+
+            // Save to db
+            return await _orderRepository.CreateOrderAsync(order);
         }
 
         public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
