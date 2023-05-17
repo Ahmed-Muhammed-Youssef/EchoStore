da57c7011914fe79d5d28c64c3d237c384302aeb
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-05-17 03:01:41 +0300

    Add cookie service (#17)
    to handle the creation of the claims principal

---
+++ Infrastructure/Services/CookieService.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Identity;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Infrastructure.Services
+{
+    public class CookieService : ICookieService
+    {
+        public ClaimsPrincipal GetPrincipal(AppUser user, string authenticationType)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.GivenName, user.DisplayName),
+                new Claim(ClaimTypes.Email , user.Email)                
+            };
+            var calimsIdentity = new ClaimsIdentity(claims, authenticationType);
+
+            var prinipal = new ClaimsPrincipal(calimsIdentity);
+            return prinipal;
+        }
+    }
+}
