deb81ccbc9c9e20dc622cc0350ee9152ea92b239
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-05-24 18:26:03 +0300

    Add Brands Controller (#28)

---
+++ API/Controllers/BrandsController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using Infrastructure.Data;
+using API.Helpers;
+using Core.Interfaces;
+using System;
+
+namespace API.Controllers
+{
+    /// <summary>
+    ///  Crud APIs for brands
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BrandsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        // GET: api/brands
+        [Cached(1000)]
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            var brands = await _unitOfWork.BrandRepository.GetBrandsAsync();
+            return Ok(brands);
+        }
+        // GET: api/brands/5
+        [Cached(1000)]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductBrand>> GetProductBrand(int id)
+        {
+            var productBrand = await _unitOfWork.BrandRepository.GetBrandAsync(id);
+
+            if (productBrand == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productBrand);
+        }
+        // DELETE: api/brands/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProductBrand(int id)
+        {
+            var brand = await _unitOfWork.BrandRepository.GetBrandAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.BrandRepository.DeleteBrand(brand);
+            var res = await _unitOfWork.Complete();
+            if (res <= 0)
+            {
+                throw new Exception("Transaction error");
+            }
+            return NoContent();
+        }
+        // POST: api/brands
+        [HttpPost("brands")]
+        public async Task<ActionResult<ProductBrand>> AddProductBrands(ProductBrand newProductBrand)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data");
+            }
+            var brand = await _unitOfWork.BrandRepository.AddProductBrandAsync(newProductBrand);
+            var res = await _unitOfWork.Complete();
+            if (res <= 0)
+            {
+                throw new Exception("Transaction error");
+            }
+            return Ok(brand);
+        }
+        // PUT: api/brands
+        [HttpPut("brands")]
+        public async Task<ActionResult<ProductBrand>> UpdateBrand(ProductBrand brand)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            if (await _unitOfWork.BrandRepository.GetBrandAsync(brand.Id) == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.BrandRepository.UpdateBrand(brand);
+            var res = await _unitOfWork.Complete();
+            if (res <= 0)
+            {
+                throw new Exception("Transaction error");
+            }
+            return Ok(brand);
+        }
+    }
+}
