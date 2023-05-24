b9b9d3648a8df631ca61f8a5e77ca0d136fe9586
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-05-24 19:08:24 +0300

    Add users seeding (#29)

--- Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -11,14 +11,22 @@ namespace Infrastructure.Identity
         {
             if(!userManager.Users.Any())
             {
+                var admin = new AppUser
+                {
+                    DisplayName = "admin",
+                    Email = "admin@admin.admin",
+                    UserName = "admin",
+                    Address = new Address {FirstName="admin", LastName="admin", Street="12 test", City="test", State="NYC", Zipcode="19121" }
+                };
                 var user = new AppUser
                 {
-                    DisplayName = "Klod",
-                    Email = "klod@test",
-                    UserName = "klod",
-                    Address = new Address {FirstName="klod", LastName="mo", Street="12 tele", City="benha", State="QY", Zipcode="19121" }
+                    DisplayName = "user",
+                    Email = "user@test",
+                    UserName = "user",
+                    Address = new Address { FirstName = "user", LastName = "user", Street = "12 test", City = "test", State = "NYC", Zipcode = "19121" }
                 };
                 await userManager.CreateAsync(user, "P@$$w0rd");
+                await userManager.CreateAsync(admin, "P@$$w0rd");
             }
         }
     }
