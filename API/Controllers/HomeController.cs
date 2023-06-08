345b5ab9b62cf9b79fa034b25396f635d10a9a19
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-06-08 16:52:00 +0300

    Add `HomeController` (#35)

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Asp.Models;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Pagination;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Asp.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public HomeController(IUnitOfWork unitOfWork)
+    {
+        this._unitOfWork = unitOfWork;
+    }
+
+    public ActionResult<IReadOnlyList<ProductInfo>> Index()
+    {
+        var paginationInfo = new PaginationInfo();
+        
+        var products =  _unitOfWork.ProductRepository.GetProductsInfo(
+            filter: p => true,
+            orderBy: null,
+            paginationInfo: ref paginationInfo
+        );
+        return View(products);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
